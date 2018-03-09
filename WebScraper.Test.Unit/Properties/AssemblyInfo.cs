c3007e0561b48ab46f58eec8d76314a1c72aadb7
Kevin <kevincrisanto@gmail.com>
2018-03-09 01:28:38 -0800

    Added Unit Tests

--- WebScraper.Test.Unit/Properties/AssemblyInfo.cs
+++ WebScraper.Test.Unit/Properties/AssemblyInfo.cs
@@ -5,15 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebScraper")]
+[assembly: AssemblyTitle("WebScraper.Test.Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebScraper")]
+[assembly: AssemblyProduct("WebScraper.Test.Unit")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("WebScraper.Test.Unit")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("646266db-7b4f-44c7-804e-30b3946e3a5b")]
+[assembly: Guid("8e5a1b9f-6c82-45af-ad55-e8736f8f8743")]
 
 // Version information for an assembly consists of the following four values:
 //
