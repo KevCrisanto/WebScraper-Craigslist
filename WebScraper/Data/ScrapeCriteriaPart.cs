1ae7facfccb72aa5fbf69abcdaa223bf1f988534
Kevin <kevincrisanto@gmail.com>
2018-03-08 21:54:51 -0800

    Add initial files

---
+++ WebScraper/Data/ScrapeCriteriaPart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebScraper.Data
+{
+    class ScrapeCriteriaPart
+    {
+        public string Regex { set; get; }
+        public RegexOptions RegexOption { set; get; }
+    }
+}
